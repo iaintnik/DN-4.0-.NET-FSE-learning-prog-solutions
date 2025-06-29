e4ef6b7ef2c42a48e1e6f533e3a6893798d261a7
iaintnik <nitishbehera321@gmail.com>
2025-06-30 01:20:28 +0530

    week1 submission

--- Week_1_Design Patterns and Principles/Exercise_1_Singleton Pattern/code/Logger.cs
+++ Week_1_Design Patterns and Principles/Exercise_1_Singleton Pattern/code/Logger.cs
@@ -1,3 +1,4 @@
+//week1
 using System;
 
 public class Logger
--- Week_1_Design Patterns and Principles/Exercise_1_Singleton Pattern/code/obj/Debug/net8.0/code.AssemblyInfo.cs
+++ Week_1_Design Patterns and Principles/Exercise_1_Singleton Pattern/code/obj/Debug/net8.0/code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bd1b6669693f46f438eb02a4495c44f7b1e0219")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8788bfccbee48a82733e393dc007fb307585a45")]
 [assembly: System.Reflection.AssemblyProductAttribute("code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
